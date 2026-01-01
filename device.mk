4d1901e88e08a0c5c8a0f8266e5afbe28397bed3
Harshit Jain <reach@harsh1998.dev>
2026-01-01 14:59:43 +0530

    nabu: build dolby atmos
    * conditionally enable dolby if it exists.
    * address a few denials while at it.
    
    Signed-off-by: Harshit Jain <reach@harsh1998.dev>

--- device.mk
+++ device.mk
@@ -24,6 +24,13 @@ WITHOUT_RESERVED_SIZE=true
 WITH_GMS=true
 endif
 
+# Conditionally inherit Dolby & disable audioFX.
+ifneq ("$(wildcard hardware/dolby/dolby.mk)", "")
+$(warning Enabling Dolby audio effect.)
+$(call inherit-product, hardware/dolby/dolby.mk)
+TARGET_EXCLUDES_AUDIOFX := true
+endif
+
 # Conditionally inherit Viper4AndroidFX & disable audioFX.
 ifneq ("$(wildcard packages/apps/ViPER4AndroidFX/config.mk)", "")
 $(warning Enabling Viper4Android audio effect.)
