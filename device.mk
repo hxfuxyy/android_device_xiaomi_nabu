c8425626bb28b07e7a39a4ece031d07d18757947
Harshit Jain <reach@harsh1998.dev>
2026-07-06 20:19:11 +0530

    nabu: build android.hardware.health-service.qti_recovery

--- device.mk
+++ device.mk
@@ -295,6 +295,7 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery \
     android.hardware.health@2.1.vendor
 
 # HIDL
